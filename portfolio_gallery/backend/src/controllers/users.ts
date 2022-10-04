37fc440b69aed6951f48dcc04e7f577e0b4313e8
Koichi <koichi89592@gmail.com>
2022-10-03 23:34:12 -0700

    [fix] organize codes and files

--- portfolio_gallery/backend/src/controllers/users.ts
+++ portfolio_gallery/backend/src/controllers/users.ts
@@ -5,6 +5,7 @@ import { Request, Response } from 'express';
 const User = mongoose.model<User>('User', UserSchema);
 
 export class UsersController {
+  //@TODO: Should be re-written from TDD process
   public async createUser(req: Request, res: Response) {
     // const user = new User(req.body);
     const user = new User({ name: 'test', email: 'test@mail.com' });
@@ -12,6 +13,7 @@ export class UsersController {
     res.status(201).send(user);
   }
 
+  //@TODO: Remove after checking basic connection from browser to database
   public async fetchUsers(req: Request, res: Response) {
     const users = await User.find({});
     res.status(200).send(users);
--- portfolio_gallery/backend/src/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var app = (0, express_1["default"])();
-var port = 8000;
-app.get('/', function (_, res) {
-    res.status(200).send();
-});
-app.listen(port, function () { return console.log("Running on port ".concat(port)); });
